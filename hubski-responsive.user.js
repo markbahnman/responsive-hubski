739e374e82115e0272ceea3c3d3b1be5129f400c
Mark Bahnman <mark.bahnman@gmail.com>
2013-02-17 22:29:38 -0800

    Init hubski-responsive.user.js

---
+++ hubski-responsive.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name          Responsive Hubski
+// @namespace     http://markbahnman.github.com/
+// @description   Adds responsive features to hubski.com
+// @copyright     Mark Bahnman 2013
+// @license       MIT
+// @author        markbahnman
+// @include       http://hubski.com/*
+// @include       https://hubski.com/*
+// @match         http://hubski.com/*
+// @match         https://hubski.com/*
+// @version       0.1
+// ==/UserScript==
+
+(function() {
+$('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+})();
